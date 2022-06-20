38a9a258f15b69b4b108d586b8341c76c8606136
João Nogueira <jrnogueira@edu.ulisboa.pt>
2022-06-20 20:09:04 +0100

    Fix l10n

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -94,17 +94,599 @@ abstract class AppLocalizations {
     Locale('pt')
   ];
 
-  /// No description provided for @oceansDoWhat.
+  /// No description provided for @whalesDoWhat.
   ///
   /// In en, this message translates to:
-  /// **'The oceans\ndo what?'**
-  String get oceansDoWhat;
+  /// **'Whales\ndo what?'**
+  String get whalesDoWhat;
 
   /// No description provided for @start.
   ///
   /// In en, this message translates to:
-  /// **'Start!'**
+  /// **'Start'**
   String get start;
+
+  /// No description provided for @sources.
+  ///
+  /// In en, this message translates to:
+  /// **'Sources'**
+  String get sources;
+
+  /// No description provided for @youreHere.
+  ///
+  /// In en, this message translates to:
+  /// **'You\'re\nhere'**
+  String get youreHere;
+
+  /// No description provided for @twoLeggedCreatures.
+  ///
+  /// In en, this message translates to:
+  /// **'Two-legged\nCreatures'**
+  String get twoLeggedCreatures;
+
+  /// No description provided for @populationOverTime.
+  ///
+  /// In en, this message translates to:
+  /// **'Blue Whale population\nover time'**
+  String get populationOverTime;
+
+  /// No description provided for @commercialWhalingPeak.
+  ///
+  /// In en, this message translates to:
+  /// **'Commercial\nwhaling\npeak'**
+  String get commercialWhalingPeak;
+
+  /// No description provided for @nearExtinction.
+  ///
+  /// In en, this message translates to:
+  /// **'Near-extinction'**
+  String get nearExtinction;
+
+  /// No description provided for @commercialWhalingBan.
+  ///
+  /// In en, this message translates to:
+  /// **'Commercial\nwhaling ban'**
+  String get commercialWhalingBan;
+
+  /// No description provided for @numbersSlowlyRising.
+  ///
+  /// In en, this message translates to:
+  /// **'Number are\nslowly rising!'**
+  String get numbersSlowlyRising;
+
+  /// No description provided for @whenBorn.
+  ///
+  /// In en, this message translates to:
+  /// **'When were you born?'**
+  String get whenBorn;
+
+  /// No description provided for @notLongUntilBan.
+  ///
+  /// In en, this message translates to:
+  /// **'Not long until\nthe whaling ban.'**
+  String get notLongUntilBan;
+
+  /// No description provided for @populationsGrowSoon.
+  ///
+  /// In en, this message translates to:
+  /// **'Whale populations\nwill start to grow soon :)'**
+  String get populationsGrowSoon;
+
+  /// No description provided for @populationsRecovering.
+  ///
+  /// In en, this message translates to:
+  /// **'Whale populations\nare already recovering :D'**
+  String get populationsRecovering;
+
+  /// No description provided for @carbon.
+  ///
+  /// In en, this message translates to:
+  /// **'Carbon'**
+  String get carbon;
+
+  /// No description provided for @oxygen.
+  ///
+  /// In en, this message translates to:
+  /// **'Oxygen'**
+  String get oxygen;
+
+  /// No description provided for @breathing.
+  ///
+  /// In en, this message translates to:
+  /// **'breathing'**
+  String get breathing;
+
+  /// No description provided for @feeding.
+  ///
+  /// In en, this message translates to:
+  /// **'feeding'**
+  String get feeding;
+
+  /// No description provided for @phytoplankton.
+  ///
+  /// In en, this message translates to:
+  /// **'fitoplâncton'**
+  String get phytoplankton;
+
+  /// No description provided for @chooseCountry.
+  ///
+  /// In en, this message translates to:
+  /// **'Choose your country:'**
+  String get chooseCountry;
+
+  /// No description provided for @pooIsImportant.
+  ///
+  /// In en, this message translates to:
+  /// **'My poo\n is important?!'**
+  String get pooIsImportant;
+
+  /// No description provided for @emitted.
+  ///
+  /// In en, this message translates to:
+  /// **'Emitted'**
+  String get emitted;
+
+  /// No description provided for @absorbed.
+  ///
+  /// In en, this message translates to:
+  /// **'Absorbed'**
+  String get absorbed;
+
+  /// No description provided for @tonsPerYear.
+  ///
+  /// In en, this message translates to:
+  /// **'(in tons / per year)'**
+  String get tonsPerYear;
+
+  /// No description provided for @worldwide.
+  ///
+  /// In en, this message translates to:
+  /// **'worldwide'**
+  String get worldwide;
+
+  /// No description provided for @billion.
+  ///
+  /// In en, this message translates to:
+  /// **'billion'**
+  String get billion;
+
+  /// No description provided for @trees.
+  ///
+  /// In en, this message translates to:
+  /// **'trees'**
+  String get trees;
+
+  /// No description provided for @equivalent.
+  ///
+  /// In en, this message translates to:
+  /// **'Equivalent to\n4 Amazon'**
+  String get equivalent;
+
+  /// No description provided for @amazonForests.
+  ///
+  /// In en, this message translates to:
+  /// **'4 Amazon\nForests!'**
+  String get amazonForests;
+
+  /// No description provided for @blueWhale.
+  ///
+  /// In en, this message translates to:
+  /// **'Blue Whale'**
+  String get blueWhale;
+
+  /// No description provided for @human.
+  ///
+  /// In en, this message translates to:
+  /// **'Human'**
+  String get human;
+
+  /// No description provided for @brachiosaurus.
+  ///
+  /// In en, this message translates to:
+  /// **'Brachiosaurus'**
+  String get brachiosaurus;
+
+  /// No description provided for @greatWhiteShake.
+  ///
+  /// In en, this message translates to:
+  /// **'Great white shark'**
+  String get greatWhiteShake;
+
+  /// No description provided for @bus.
+  ///
+  /// In en, this message translates to:
+  /// **'Bus'**
+  String get bus;
+
+  /// No description provided for @you.
+  ///
+  /// In en, this message translates to:
+  /// **'You!'**
+  String get you;
+
+  /// No description provided for @hadNoIdea.
+  ///
+  /// In en, this message translates to:
+  /// **'I had no idea!'**
+  String get hadNoIdea;
+
+  /// No description provided for @carbonDeepWhenTheyDie.
+  ///
+  /// In en, this message translates to:
+  /// **'Carbon (CO₂) shuttled to the deep\nsea by baleen whales when they die'**
+  String get carbonDeepWhenTheyDie;
+
+  /// No description provided for @whalesToday.
+  ///
+  /// In en, this message translates to:
+  /// **'Whales\nToday'**
+  String get whalesToday;
+
+  /// No description provided for @ifWhalesRecovered.
+  ///
+  /// In en, this message translates to:
+  /// **'If whale\npopulations\nrecovered to\npre-whaling\nnumbers'**
+  String get ifWhalesRecovered;
+
+  /// No description provided for @equivalentToCO2.
+  ///
+  /// In en, this message translates to:
+  /// **'That\'s equivalent to\nthe CO2 absorbed by:'**
+  String get equivalentToCO2;
+
+  /// No description provided for @doOurPart.
+  ///
+  /// In en, this message translates to:
+  /// **'But we must do our part!'**
+  String get doOurPart;
+
+  /// No description provided for @touchToFlip.
+  ///
+  /// In en, this message translates to:
+  /// **'touch to flip'**
+  String get touchToFlip;
+
+  /// No description provided for @ourPart1Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Reduce food waste'**
+  String get ourPart1Label;
+
+  /// No description provided for @ourPart1Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Food waste is responsible for 6% of total global emissions'**
+  String get ourPart1Text;
+
+  /// No description provided for @ourPart2Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Eat a Plant-rich diet'**
+  String get ourPart2Label;
+
+  /// No description provided for @ourPart2Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Plant-rich diets reduce emissions and also tend to be healtier'**
+  String get ourPart2Text;
+
+  /// No description provided for @ourPart3Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Make your home more efficient'**
+  String get ourPart3Label;
+
+  /// No description provided for @ourPart3Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Weatherize, use led lights, be conscious of energy use'**
+  String get ourPart3Text;
+
+  /// No description provided for @ourPart4Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Shift to alternative transportation'**
+  String get ourPart4Label;
+
+  /// No description provided for @ourPart4Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Public transportation, Bicycles, Car-pool, walk'**
+  String get ourPart4Text;
+
+  /// No description provided for @ourPart5Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Buyn less stuff'**
+  String get ourPart5Label;
+
+  /// No description provided for @ourPart5Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Try to refurbish or repurpose existing items'**
+  String get ourPart5Text;
+
+  /// No description provided for @ourPart6Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Support equality'**
+  String get ourPart6Label;
+
+  /// No description provided for @ourPart6Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Access to high-quality, inclusive education and voluntary reproductive'**
+  String get ourPart6Text;
+
+  /// No description provided for @krill.
+  ///
+  /// In en, this message translates to:
+  /// **'Krill'**
+  String get krill;
+
+  /// No description provided for @microplastics.
+  ///
+  /// In en, this message translates to:
+  /// **'Microplastics'**
+  String get microplastics;
+
+  /// No description provided for @plasticWasteCreate.
+  ///
+  /// In en, this message translates to:
+  /// **'Plastic waste created'**
+  String get plasticWasteCreate;
+
+  /// No description provided for @whereYouFrom.
+  ///
+  /// In en, this message translates to:
+  /// **'Where are you from?'**
+  String get whereYouFrom;
+
+  /// No description provided for @tons.
+  ///
+  /// In en, this message translates to:
+  /// **'tons'**
+  String get tons;
+
+  /// No description provided for @adultAfricanElephants.
+  ///
+  /// In en, this message translates to:
+  /// **'Adult African\nElephants'**
+  String get adultAfricanElephants;
+
+  /// No description provided for @whatCanYouDo.
+  ///
+  /// In en, this message translates to:
+  /// **'What can\nyou do?'**
+  String get whatCanYouDo;
+
+  /// No description provided for @canDo1Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Take a reusable bag with you'**
+  String get canDo1Label;
+
+  /// No description provided for @canDo2Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Respect natural environments'**
+  String get canDo2Label;
+
+  /// No description provided for @canDo2Text.
+  ///
+  /// In en, this message translates to:
+  /// **'and leave them cleaner then you found them'**
+  String get canDo2Text;
+
+  /// No description provided for @canDo3Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Look for natural fibres'**
+  String get canDo3Label;
+
+  /// No description provided for @canDo3Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Synthetic fibres like polyester & nylon release plastic microfibers when washed'**
+  String get canDo3Text;
+
+  /// No description provided for @canDo4Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Stop using single-use plastics'**
+  String get canDo4Label;
+
+  /// No description provided for @canDo5Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Buy vegies & fruit not wrapped in plastic'**
+  String get canDo5Label;
+
+  /// No description provided for @canDo6Label.
+  ///
+  /// In en, this message translates to:
+  /// **'When you can\'t really avoid using plastic, recycle it after'**
+  String get canDo6Label;
+
+  /// No description provided for @baltazarMigration.
+  ///
+  /// In en, this message translates to:
+  /// **'Baltazar\'s Migration\n6,400 km/year'**
+  String get baltazarMigration;
+
+  /// No description provided for @feedingGrounds.
+  ///
+  /// In en, this message translates to:
+  /// **'feeding grounds'**
+  String get feedingGrounds;
+
+  /// No description provided for @breedingGround.
+  ///
+  /// In en, this message translates to:
+  /// **'breeding grounds'**
+  String get breedingGround;
+
+  /// No description provided for @clickToSeeDistance.
+  ///
+  /// In en, this message translates to:
+  /// **'Click to see distance\nfrom where you are from'**
+  String get clickToSeeDistance;
+
+  /// No description provided for @migrationCorridors.
+  ///
+  /// In en, this message translates to:
+  /// **'Blue whales\nmigration corridors'**
+  String get migrationCorridors;
+
+  /// No description provided for @shippingDensity.
+  ///
+  /// In en, this message translates to:
+  /// **'Shipping density'**
+  String get shippingDensity;
+
+  /// No description provided for @slide.
+  ///
+  /// In en, this message translates to:
+  /// **'slide'**
+  String get slide;
+
+  /// No description provided for @weNeedTo.
+  ///
+  /// In en, this message translates to:
+  /// **'We need to...'**
+  String get weNeedTo;
+
+  /// No description provided for @needTo1Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Slow ships down'**
+  String get needTo1Label;
+
+  /// No description provided for @needTo1Text.
+  ///
+  /// In en, this message translates to:
+  /// **'especially in the migration corridors'**
+  String get needTo1Text;
+
+  /// No description provided for @needTo2Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Avoid certain areas at particular periods'**
+  String get needTo2Label;
+
+  /// No description provided for @needTo2Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Adapt to marine life patterns'**
+  String get needTo2Text;
+
+  /// No description provided for @needTo3Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Buy local products'**
+  String get needTo3Label;
+
+  /// No description provided for @needTo3Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Support local economies and avoid long transport'**
+  String get needTo3Text;
+
+  /// No description provided for @noiceVolume.
+  ///
+  /// In en, this message translates to:
+  /// **'Noise volume'**
+  String get noiceVolume;
+
+  /// No description provided for @realOceanNoice.
+  ///
+  /// In en, this message translates to:
+  /// **'(real ocean noise)'**
+  String get realOceanNoice;
+
+  /// No description provided for @soundVolumes.
+  ///
+  /// In en, this message translates to:
+  /// **'Sound volumes'**
+  String get soundVolumes;
+
+  /// No description provided for @clickOnIcons.
+  ///
+  /// In en, this message translates to:
+  /// **'Click on\nthe icons'**
+  String get clickOnIcons;
+
+  /// No description provided for @decibels.
+  ///
+  /// In en, this message translates to:
+  /// **'decibels (dB)'**
+  String get decibels;
+
+  /// No description provided for @reduceNoise.
+  ///
+  /// In en, this message translates to:
+  /// **'Let\'s reduce\nthe noise'**
+  String get reduceNoise;
+
+  /// No description provided for @lets1Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Less noisy vessels'**
+  String get lets1Label;
+
+  /// No description provided for @lets1Text.
+  ///
+  /// In en, this message translates to:
+  /// **'For example, replace propellers'**
+  String get lets1Text;
+
+  /// No description provided for @lets2Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Avoid sensitive areas'**
+  String get lets2Label;
+
+  /// No description provided for @lets2Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Adapt to ocean life'**
+  String get lets2Text;
+
+  /// No description provided for @lets3Label.
+  ///
+  /// In en, this message translates to:
+  /// **'Alternative Energy'**
+  String get lets3Label;
+
+  /// No description provided for @lets3Text.
+  ///
+  /// In en, this message translates to:
+  /// **'Support renewables instead of oil drilling'**
+  String get lets3Text;
+
+  /// No description provided for @sameCO2Captured.
+  ///
+  /// In en, this message translates to:
+  /// **'The same CO₂\n captured by'**
+  String get sameCO2Captured;
+
+  /// No description provided for @treesInOnYear.
+  ///
+  /// In en, this message translates to:
+  /// **'trees in\none year'**
+  String get treesInOnYear;
+
+  /// No description provided for @letsDoThis.
+  ///
+  /// In en, this message translates to:
+  /// **'Let\'s do this\ntogether!'**
+  String get letsDoThis;
+
+  /// No description provided for @bePartOfThisStory.
+  ///
+  /// In en, this message translates to:
+  /// **'And now, leave your mark on our walls.\nBe part of this story. :)'**
+  String get bePartOfThisStory;
 }
 
 class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -8,8 +8,299 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get oceansDoWhat => 'The oceans\ndo what?';
+  String get whalesDoWhat => 'Whales\ndo what?';
 
   @override
-  String get start => 'Start!';
+  String get start => 'Start';
+
+  @override
+  String get sources => 'Sources';
+
+  @override
+  String get youreHere => 'You\'re\nhere';
+
+  @override
+  String get twoLeggedCreatures => 'Two-legged\nCreatures';
+
+  @override
+  String get populationOverTime => 'Blue Whale population\nover time';
+
+  @override
+  String get commercialWhalingPeak => 'Commercial\nwhaling\npeak';
+
+  @override
+  String get nearExtinction => 'Near-extinction';
+
+  @override
+  String get commercialWhalingBan => 'Commercial\nwhaling ban';
+
+  @override
+  String get numbersSlowlyRising => 'Number are\nslowly rising!';
+
+  @override
+  String get whenBorn => 'When were you born?';
+
+  @override
+  String get notLongUntilBan => 'Not long until\nthe whaling ban.';
+
+  @override
+  String get populationsGrowSoon => 'Whale populations\nwill start to grow soon :)';
+
+  @override
+  String get populationsRecovering => 'Whale populations\nare already recovering :D';
+
+  @override
+  String get carbon => 'Carbon';
+
+  @override
+  String get oxygen => 'Oxygen';
+
+  @override
+  String get breathing => 'breathing';
+
+  @override
+  String get feeding => 'feeding';
+
+  @override
+  String get phytoplankton => 'fitoplâncton';
+
+  @override
+  String get chooseCountry => 'Choose your country:';
+
+  @override
+  String get pooIsImportant => 'My poo\n is important?!';
+
+  @override
+  String get emitted => 'Emitted';
+
+  @override
+  String get absorbed => 'Absorbed';
+
+  @override
+  String get tonsPerYear => '(in tons / per year)';
+
+  @override
+  String get worldwide => 'worldwide';
+
+  @override
+  String get billion => 'billion';
+
+  @override
+  String get trees => 'trees';
+
+  @override
+  String get equivalent => 'Equivalent to\n4 Amazon';
+
+  @override
+  String get amazonForests => '4 Amazon\nForests!';
+
+  @override
+  String get blueWhale => 'Blue Whale';
+
+  @override
+  String get human => 'Human';
+
+  @override
+  String get brachiosaurus => 'Brachiosaurus';
+
+  @override
+  String get greatWhiteShake => 'Great white shark';
+
+  @override
+  String get bus => 'Bus';
+
+  @override
+  String get you => 'You!';
+
+  @override
+  String get hadNoIdea => 'I had no idea!';
+
+  @override
+  String get carbonDeepWhenTheyDie => 'Carbon (CO₂) shuttled to the deep\nsea by baleen whales when they die';
+
+  @override
+  String get whalesToday => 'Whales\nToday';
+
+  @override
+  String get ifWhalesRecovered => 'If whale\npopulations\nrecovered to\npre-whaling\nnumbers';
+
+  @override
+  String get equivalentToCO2 => 'That\'s equivalent to\nthe CO2 absorbed by:';
+
+  @override
+  String get doOurPart => 'But we must do our part!';
+
+  @override
+  String get touchToFlip => 'touch to flip';
+
+  @override
+  String get ourPart1Label => 'Reduce food waste';
+
+  @override
+  String get ourPart1Text => 'Food waste is responsible for 6% of total global emissions';
+
+  @override
+  String get ourPart2Label => 'Eat a Plant-rich diet';
+
+  @override
+  String get ourPart2Text => 'Plant-rich diets reduce emissions and also tend to be healtier';
+
+  @override
+  String get ourPart3Label => 'Make your home more efficient';
+
+  @override
+  String get ourPart3Text => 'Weatherize, use led lights, be conscious of energy use';
+
+  @override
+  String get ourPart4Label => 'Shift to alternative transportation';
+
+  @override
+  String get ourPart4Text => 'Public transportation, Bicycles, Car-pool, walk';
+
+  @override
+  String get ourPart5Label => 'Buyn less stuff';
+
+  @override
+  String get ourPart5Text => 'Try to refurbish or repurpose existing items';
+
+  @override
+  String get ourPart6Label => 'Support equality';
+
+  @override
+  String get ourPart6Text => 'Access to high-quality, inclusive education and voluntary reproductive';
+
+  @override
+  String get krill => 'Krill';
+
+  @override
+  String get microplastics => 'Microplastics';
+
+  @override
+  String get plasticWasteCreate => 'Plastic waste created';
+
+  @override
+  String get whereYouFrom => 'Where are you from?';
+
+  @override
+  String get tons => 'tons';
+
+  @override
+  String get adultAfricanElephants => 'Adult African\nElephants';
+
+  @override
+  String get whatCanYouDo => 'What can\nyou do?';
+
+  @override
+  String get canDo1Label => 'Take a reusable bag with you';
+
+  @override
+  String get canDo2Label => 'Respect natural environments';
+
+  @override
+  String get canDo2Text => 'and leave them cleaner then you found them';
+
+  @override
+  String get canDo3Label => 'Look for natural fibres';
+
+  @override
+  String get canDo3Text => 'Synthetic fibres like polyester & nylon release plastic microfibers when washed';
+
+  @override
+  String get canDo4Label => 'Stop using single-use plastics';
+
+  @override
+  String get canDo5Label => 'Buy vegies & fruit not wrapped in plastic';
+
+  @override
+  String get canDo6Label => 'When you can\'t really avoid using plastic, recycle it after';
+
+  @override
+  String get baltazarMigration => 'Baltazar\'s Migration\n6,400 km/year';
+
+  @override
+  String get feedingGrounds => 'feeding grounds';
+
+  @override
+  String get breedingGround => 'breeding grounds';
+
+  @override
+  String get clickToSeeDistance => 'Click to see distance\nfrom where you are from';
+
+  @override
+  String get migrationCorridors => 'Blue whales\nmigration corridors';
+
+  @override
+  String get shippingDensity => 'Shipping density';
+
+  @override
+  String get slide => 'slide';
+
+  @override
+  String get weNeedTo => 'We need to...';
+
+  @override
+  String get needTo1Label => 'Slow ships down';
+
+  @override
+  String get needTo1Text => 'especially in the migration corridors';
+
+  @override
+  String get needTo2Label => 'Avoid certain areas at particular periods';
+
+  @override
+  String get needTo2Text => 'Adapt to marine life patterns';
+
+  @override
+  String get needTo3Label => 'Buy local products';
+
+  @override
+  String get needTo3Text => 'Support local economies and avoid long transport';
+
+  @override
+  String get noiceVolume => 'Noise volume';
+
+  @override
+  String get realOceanNoice => '(real ocean noise)';
+
+  @override
+  String get soundVolumes => 'Sound volumes';
+
+  @override
+  String get clickOnIcons => 'Click on\nthe icons';
+
+  @override
+  String get decibels => 'decibels (dB)';
+
+  @override
+  String get reduceNoise => 'Let\'s reduce\nthe noise';
+
+  @override
+  String get lets1Label => 'Less noisy vessels';
+
+  @override
+  String get lets1Text => 'For example, replace propellers';
+
+  @override
+  String get lets2Label => 'Avoid sensitive areas';
+
+  @override
+  String get lets2Text => 'Adapt to ocean life';
+
+  @override
+  String get lets3Label => 'Alternative Energy';
+
+  @override
+  String get lets3Text => 'Support renewables instead of oil drilling';
+
+  @override
+  String get sameCO2Captured => 'The same CO₂\n captured by';
+
+  @override
+  String get treesInOnYear => 'trees in\none year';
+
+  @override
+  String get letsDoThis => 'Let\'s do this\ntogether!';
+
+  @override
+  String get bePartOfThisStory => 'And now, leave your mark on our walls.\nBe part of this story. :)';
 }
